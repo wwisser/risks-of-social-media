65da9f4e73c6d3ff59221823d7af1cd1bbedeb2d
Wendelin Wisser <wendelin.wisser@active-agent.com>
2024-11-21 06:43:09 +0100

    DM me

--- server/src/Main.java
+++ server/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // Tell me what to do here. DM me, or fund me money fo fun!
     }
 }
