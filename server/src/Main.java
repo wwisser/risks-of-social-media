3976201de2427c2360050ce43dd2d59f124b6370
Wendelin Wisser <wendelin.wisser@active-agent.com>
2024-11-21 06:48:47 +0100

    fck

--- server/src/Main.java
+++ server/src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
+
+    static class Observer {
+        // I like nesting!
+        // In my brain is pattern recognition, it's wrong.
+        class Api {
+            String url;
+
+            void getTruncated() {
+                api.truncate();
+            }
+
+            void api.truncate(); // this is an error. gavin, fix it. elon, sign me or fuck off
+        }
+
+        public static void main(String... args) {
+
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        // Tell me what to do here. DM me, or fund me money fo fun!
     }
 }
